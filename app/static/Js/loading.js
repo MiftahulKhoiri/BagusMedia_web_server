94730d9160eabb70d223f7d9017c987a60c3531c
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-16 20:20:31 +0800

    Delete app directory

--- app/static/Js/loading.js
+++
@@ -1,9 +0,0 @@
-// Tampilkan loading
-function showLoading() {
-    document.getElementById("loading-overlay").style.display = "flex";
-}
-
-// Sembunyikan loading
-function hideLoading() {
-    document.getElementById("loading-overlay").style.display = "none";
-}
