fd22088d81d0b92bf787acad8b24187ef3cea830
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-16 12:16:28 +0800

    Create loading.js

---
+++ app/static/Js/loading.js
@@ -0,0 +1,9 @@
+// Tampilkan loading
+function showLoading() {
+    document.getElementById("loading-overlay").style.display = "flex";
+}
+
+// Sembunyikan loading
+function hideLoading() {
+    document.getElementById("loading-overlay").style.display = "none";
+}
