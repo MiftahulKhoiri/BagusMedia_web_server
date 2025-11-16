4dce13209f5e7667558e44ddda2c491e53e69222
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-16 11:35:54 +0800

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,1 +1,14 @@
-# routes peg
+from .auth_routes import register_auth_routes
+from .home_routes import register_home_routes
+from .media_routes import register_media_routes
+from .profile_routes import register_profile_routes
+from .update_routes import register_update_routes
+from .upload_routes import register_upload_routes
+
+def init_app(app, sock):
+    register_auth_routes(app)
+    register_home_routes(app)
+    register_media_routes(app)
+    register_profile_routes(app)
+    register_update_routes(app, sock)
+    register_upload_routes(app)
