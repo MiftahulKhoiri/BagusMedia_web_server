5b25296542c04fa32bb997dfa03e16ecbdb601cc
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-16 12:03:49 +0800

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,12 +1,15 @@
+from .auth_routes import register_auth_routes
 from .home_routes import register_home_routes
 from .profile_routes import register_profile_routes
+from .upload_routes import register_upload_routes
 from .media_routes import register_media_routes
 from .update_routes import register_update_routes
-from .auth_routes import register_auth_routes   # nanti kita buat
 
-def init_routes(app, sock):
+def init_app(app, sock):
+    # Register semua route terpisah
     register_auth_routes(app)
     register_home_routes(app)
     register_profile_routes(app)
+    register_upload_routes(app)
     register_media_routes(app)
     register_update_routes(app, sock)
