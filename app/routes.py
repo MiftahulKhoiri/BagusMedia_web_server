6c60f6ac7e82ab6adae8f01b80e3a1f1592e80df
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-17 01:36:30 +0800

    Add files via upload

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from routes import init_routes
+from flask_sock import Sock
+
+def create_app():
+    app = Flask(__name__)
+    sock = Sock(app)
+
+    # INI WAJIB
+    init_routes(app, sock)
+
+    return app
