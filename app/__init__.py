68d59e6f28e009d4c0cac73b9cb945fc9c299904
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-17 20:00:09 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -55,6 +55,22 @@ def create_app():
     init_db_from_path(app.config['DATABASE'])
 
     sock = Sock(app)
+# ============================================
+# REGISTER BLUEPRINTS
+# ============================================
+from .blueprints.auth import auth_bp
+from .blueprints.home import home_bp
+from .blueprints.media import media_bp
+from .blueprints.upload import upload_bp
+from .blueprints.profile import profile_bp
+from .blueprints.update import update_bp
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(home_bp)
+app.register_blueprint(media_bp)
+app.register_blueprint(upload_bp)
+app.register_blueprint(profile_bp)
+app.register_blueprint(update_bp)
 
     # Panggil routes dari folder routes
     from . import routes
