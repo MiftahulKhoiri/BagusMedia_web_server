e2f5a4fa43e358217b5a74b3c04915ebfc2714e6
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-17 21:55:05 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -107,5 +107,20 @@ def create_app():
     app.register_blueprint(update_bp)
     register_websocket(sock)
 
+from .auth import auth_bp
+from .home import home_bp
+from .media import media_bp
+from .upload import upload_bp
+from .profile import profile_bp
+from .update import update_bp
+
+__all__ = [
+    "auth_bp",
+    "home_bp",
+    "media_bp",
+    "upload_bp",
+    "profile_bp",
+    "update_bp",
+]
 
     return app
