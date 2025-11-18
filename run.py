a0b8fb95bfd7c7df17aa26d5b47d9414218dec60
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-18 19:13:52 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
-from app import create_app
-import os
-
-app = create_app()
+from app.app import app
 
 if __name__ == "__main__":
-    # pastikan instance dir ada
-    instance_dir = os.path.join(os.path.dirname(__file__), "instance")
-    os.makedirs(instance_dir, exist_ok=True)
-
     app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
