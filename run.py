3c55302bf54ceeb4fa7974010636c855c38bbde9
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-20 18:38:43 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,21 @@
+import socket
 from app import create_app
 
 app = create_app()
 
+def get_free_port(start_port=5000):
+    port = start_port
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            # cek apakah port bisa dibuka
+            if s.connect_ex(("127.0.0.1", port)) != 0:
+                return port  # port aman dipakai
+            port += 1  # kalau kepakai, lanjut port berikutnya
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    default_port = 5000
+    port = get_free_port(default_port)
+
+    print(f"Port ditemukan: {port}")
+
+    app.run(host="0.0.0.0", port=port, debug=True)
